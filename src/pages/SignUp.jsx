6190e703dc4723e49c4f17715c05071344320bc8
Dhanesh Khurana <dhaneshkhurana016@gmail.com>
2023-09-13 10:12:48 +0530

    Set up the project and created login page with router set up

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "../css/Login.css";
+import img_Social from "../img/social-desktop.PNG"
+
+function SignUp() {
+  return (
+    <div className="row d-flex flex-grow-1 align-items-center justify-content-center ">
+      {/* left side social image*/}
+      <div className="col-12 col-md-7">
+        <img className="socialImg" src={img_Social} alt="social" />
+      </div>
+      {/*Right side Login Card*/}
+      <div className="col-12 col-md-5 ">
+        <div className=" card social-form shadow">
+          <div className="card-body  justify-content-center align-items-center">
+            <h2>Log In</h2>
+            <form className="justify-content-center align-items-center">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control mt-2 "
+                  id="email"
+                  placeholder="Enter username, email or phone"
+                ></input>
+
+                <input
+                  type="text"
+                  className="form-control mt-2 "
+                  id="password"
+                  placeholder="Enter password"
+                ></input>
+                <div className="d-grid mt-3 ">
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
+            <form>
+              <div>
+                <span>
+                  <hr className="w-45" />
+                  OR
+                  <hr className="w-45" />
+                </span>
+              </div>
+              <div>
+                <span className="text-muted">
+                  Don't have an account? <a href="#">Sign Up</a>
+                </span>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
